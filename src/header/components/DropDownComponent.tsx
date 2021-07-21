e171089efd137273205a32268feb7e1682516d36
Sulav Baskota <sulav.baskota0419@gmail.com>
2021-07-21 23:13:57 +0545

    created a json object for navigation menu items.

--- src/header/components/DropDownComponent.tsx
+++ src/header/components/DropDownComponent.tsx
@@ -1,7 +1,27 @@
 import Popover from '@material-ui/core/Popover';
+import { Fragment } from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
-export default function DropDownList(props : any) {
+export default function DropDownList(props: any) {
     return (
-        <div>Hello</div>
+        <Fragment>
+            <List component="nav" aria-label="main mailbox folders"
+                style={{
+                    position: "absolute",
+                    display: "block",
+                    float: "left"
+                }}>
+                <ListItem button>
+
+                    <ListItemText primary="Inbox" />
+                </ListItem>
+                <ListItem button>
+
+                    <ListItemText primary="Drafts" />
+                </ListItem>
+            </List>
+        </Fragment>
     )
 }
